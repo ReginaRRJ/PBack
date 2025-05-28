a3cc96bc8683230d9c2591627027ec353f0fb962
ReginaRRJ <149405875+ReginaRRJ@users.noreply.github.com>
2025-05-27 18:21:39 -0600

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+import eslintPluginReact from 'eslint-plugin-react'
+
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      react: eslintPluginReact,
+    },
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
